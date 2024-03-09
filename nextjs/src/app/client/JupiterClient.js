7267aae569f58dd2e025f539724dea2d67cd4587
Liam Faruq <liam@Liams-MacBook-Pro.local>
2024-03-09 17:47:54 +0000

    add-version-tx-and-confirmations

--- nextjs/src/app/client/JupiterClient.js
+++ nextjs/src/app/client/JupiterClient.js
@@ -1,16 +1,18 @@
 import { Connection, Keypair, VersionedTransaction } from '@solana/web3.js';
+// import { TokenInfo } from "@solana/spl-token-registry"
 import fetch from 'cross-fetch';
 import { Wallet } from '@project-serum/anchor';
 import bs58 from 'bs58';
 import dotenv from 'dotenv';
-import swapConfig from '../config/SwapDevnetAddresses';
+import swapConfig from '../config/SwapConfig.js';
 
 
 // setup environment variables
 dotenv.config();
-const { SOLANA_NETWORK, PRIVATE_KEY, DEVNET_RPC } = process.env;
+const { SOLANA_NETWORK, PRIVATE_KEY, DEVNET_RPC,CHAIN_ID,MAINNET_RPC} = process.env;
 
-const RPC = DEVNET_RPC || 'https://api.devnet.solana.com';
+const RPC = MAINNET_RPC || 'https://api.devnet.solana.com';
+const chainId = CHAIN_ID;
 
 //returns
 // {
@@ -25,7 +27,7 @@ async function priceSwap(buyTkId, sellTkId, amount) {
     const priceUrl = `https://price.jup.ag/v4/price?ids=${sellTkId}&vsToken=${buyTkId}`;
     const priceResponse = await fetch(priceUrl);
     const priceData = await priceResponse.json();
-  
+
     const { id, mintSymbol, vsToken, vsTokenSymbol, price } = priceData.data[sellTkId];
   
     const sellQty = amount;
@@ -38,7 +40,7 @@ async function priceSwap(buyTkId, sellTkId, amount) {
       const usdPriceData = await usdPriceResponse.json();
   
       const buyTokenInUSD = usdPriceData.data[buyTkId].price;
-      usdPrice = price * buyTokenInUSD;
+      usdPrice = buyQty * buyTokenInUSD;
     }
   
     return {
@@ -84,74 +86,105 @@ async function priceSwap(buyTkId, sellTkId, amount) {
 //       usdPrice: 3925.144357
 //     }
   
-  async function executeSwap(swapItem){
+  async function createSwapTransactions(swapItems){
 
-    const connection = new Connection('https://neat-hidden-sanctuary.solana-mainnet.discover.quiknode.pro/2af5315d336f9ae920028bbb90a73b724dc1bbed/');
+    console.log('executing swap, on chain', chainId, 'with rpc', RPC);
     const wallet = new Wallet(Keypair.fromSecretKey(bs58.decode(PRIVATE_KEY || '')));
+
+    const transactions = await Promise.all(swapItems.map(async (swapItem) => {
+        const sellTokenInfo = await getTokenInfo(chainId, swapItem.sellTkId);
+        const buyTokenInfo = await getTokenInfo(chainId, swapItem.buyTkId);
     
-    //pass in platformFeeBps as a parameter in the quote.
-    const quoteResponse = await (
-        await fetch(`https://quote-api.jup.ag/v6/quote?inputMint=${swapConfig[swapItem.sellTkId]}\
-        &outputMint=${swapConfig[swapItem.buyTkId]}\
-        &amount=${swapItem.buyQty}\
-        &slippageBps=50`
-        )
-      ).json();
-      console.log({ quoteResponse });
+        console.log({ sellTokenInfo, buyTokenInfo });
 
+    
+        //pass in platformFeeBps as a parameter in the quote.
+    
+        const quoteUrl =`https://quote-api.jup.ag/v6/quote?inputMint=${sellTokenInfo.address}&outputMint=${buyTokenInfo.address}&amount=${swapItem.buyQty * 10 ^ buyTokenInfo.decimals}&slippageBps=50`;
+        const quoteResponse = await (await fetch(quoteUrl)).json();
+          
+        console.log({ quoteResponse });
+    
+          // get serialized transactions for the swap
+            const { swapTransaction } = await (
+                await fetch('https://quote-api.jup.ag/v6/swap', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    // quoteResponse from /quote api
+                    quoteResponse,
+                    // user public key to be used for the swap
+                    userPublicKey: wallet.publicKey.toString(),
+                    // auto wrap and unwrap SOL. default is true
+                    wrapAndUnwrapSol: true,
+                    prioritizationFeeLamports: 'auto'
+                    // feeAccount is optional. Use if you want to charge a fee.  feeBps must have been passed in /quote API.
+                    // feeAccount: "fee_account_public_key"
+                })
+                })
+            ).json();
+    
+            console.log({ swapTransaction });
+    
+    
+            // deserialize the transaction
+            const swapTransactionBuf = Buffer.from(swapTransaction, 'base64');
+            var transaction = VersionedTransaction.deserialize(swapTransactionBuf);
+            console.log(transaction);
 
-      // get serialized transactions for the swap
-        const { swapTransaction } = await (
-            await fetch('https://quote-api.jup.ag/v6/swap', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                // quoteResponse from /quote api
-                quoteResponse,
-                // user public key to be used for the swap
-                userPublicKey: wallet.publicKey.toString(),
-                // auto wrap and unwrap SOL. default is true
-                wrapAndUnwrapSol: true,
-                // feeAccount is optional. Use if you want to charge a fee.  feeBps must have been passed in /quote API.
-                // feeAccount: "fee_account_public_key"
-            })
-            })
-        ).json();
+            return transaction;
+    }));
 
-        console.log({ swapTransaction });
-
-
-        // // deserialize the transaction
-        // const swapTransactionBuf = Buffer.from(swapTransaction, 'base64');
-        // var transaction = VersionedTransaction.deserialize(swapTransactionBuf);
-        // console.log(transaction);
-
+    console.dir(transactions, { depth: null });
+    return transactions;
         // // sign the transaction
         // transaction.sign([wallet.payer]);
 
         // // Execute the transaction
         // const rawTransaction = transaction.serialize()
-        // const txid = await connection.sendRawTransaction(rawTransaction, {
+        // const signature = await connection.sendRawTransaction(rawTransaction, {
         // skipPreflight: true,
         // maxRetries: 2
         // });
-        // await connection.confirmTransaction(txid);
-        // console.log(`https://solscan.io/tx/${txid}`);
+        // console.log("--------------------_>", signature);
+
+        // // const blockheight = await connection.getBlockHeight();
+
+        // // await connection.confirmTransaction(txId);
+        // await connection.confirmTransaction({signature, blockhash, lastValidBlockHeight});
+
+        // console.log(`https://solscan.io/tx/${signature}`);
+}
+
+async function confirmTransactions(transactions, signature){
+    const connection = new Connection(RPC, 'confirmed');
+    const { blockhash, lastValidBlockHeight } = await connection.getLatestBlockhash();
+
+    await Promise.all(transactions.map(async (transaction) => {
+        await connection.confirmTransaction({signature, blockhash, lastValidBlockHeight});
+    }));
+}
+
+async function getTokenInfo(chainId, tokenId) {
+    console.log('fetching token info', chainId, tokenId);
+    const tokenInfo = await Promise.all(
+        await fetch('https://raw.githubusercontent.com/solana-labs/token-list/main/src/tokens/solana.tokenlist.json')
+    ).json();
+    return tokenInfo.tokens.find((token) => token.chainId === Number(chainId) && token.symbol === tokenId);
 }
 
 async function main(){
 
-    const swapItem = {
-        buyTkId: 'ETH',
-        buyQty: 0.186275,
-        sellTkId: 'USDC',
-        sellQty: 731.156265100175,
-        usdPrice: 3925.144357
-    };
-    
-    await executeSwap(swapItem);
+    const swapItem1 = await priceSwap('USDC', 'SOL', 0.1);
+    const swapItem2 = await priceSwap('USDC', 'SOL', 0.11);
+    const swapItem3 = await priceSwap('USDC', 'SOL', 0.12);
+
+    // console.log(swapItem);
+
+    await executeSwaps([swapItem1, swapItem2, swapItem3]);
+
 
     // const portfolio = [
     //     {
@@ -171,7 +204,9 @@ async function main(){
 
 }
 
-main();
 
 
-// export { priceSwap, pricePortfolio };
+// main();
+
+
+export { priceSwap, pricePortfolio, confirmTransactions, createSwapTransactions};
---
+++ nextjs/src/app/config/SwapConfig.js
@@ -0,0 +1,7 @@
+const swapConfig =  {
+        USDC: "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
+        SOL: "So11111111111111111111111111111111111111112",
+        ETH: "Ff5JqsAYUD4vAfQUtfRprT4nXu9e28tTBZTDFMnJNdvd"
+}
+
+export default swapConfig;
